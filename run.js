d1a7ba67713150fb7179cac7c6cef6e3fd94c220
BKleinberg <bennettkleinberg@gmail.com>
2017-06-15 16:11:21 +0200

    minor npm update

--- run.js
+++ run.js
@@ -1,6 +1,7 @@
 var netanos = require('netanos');
+// var netanos = require('./Netanos.js');
 var input = "Max and Ben spent more than 1000 hours on writing the software. They started in August 2016 in Amsterdam.";
 
-netanos.ner(input, function(output) {
+netanos.anon(input, function(output) {
     console.log(output);
 });
