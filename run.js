2c2204f93931dc8b5337f17b119f39e6c130c10a
maximilianmozes <mozes@cs.tum.edu>
2018-08-20 13:39:02 +0200

    Updated example

--- run.js
+++ run.js
@@ -2,6 +2,14 @@
 var netanos = require('./Netanos.js');
 var input = "Max and Ben spent more than 1000 hours on writing the software. They started in August 2016 in Amsterdam.";
 
-netanos.anon(input, function(output) {
+var entities = {
+    person: true,
+    organization: true,
+    currency: true,
+    date: true,
+    location: true
+};
+
+netanos.anon(input, entities, function(output) {
     console.log(output);
 });
